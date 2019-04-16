49ca6760e1483e70ed99a29935f40e25017cbfdb
liuguangqing <fee_lgq@aliyun.com>
2019-04-16 23:05:28 +0800

    台风、天气结束修改

--- serve/data/weather.js
+++ serve/data/weather.js
@@ -1,20 +1,23 @@
-module.exports = [
-    {
-        'type': 'FeatureCollection',
+module.exports = [{
+    'type': 'FeatureCollection',
+    defType: 'weather',
+    visible: true,
+    features: [{
+        'type': 'Feature',
         defType: 'weather',
-        name: '1',
-        features: [
-            {
-                'type': 'Feature',
-                'properties': {
-                    'popupContent': 'Coors Field',
-                    type: 'weather',
-                    url: 'http://www.shipxy.com/Content/js/elanemap/images/yin.png'
-                },
-                'geometry': {
-                    'type': 'Point',
-                    'coordinates': [112.5, 20.5]
-                }
-            }]
-    }
-]
+        'properties': {
+            'popupContent': 'Coors Field',
+            style: {
+                iconSize: [20, 20],
+                iconAnchor: [10, 10],
+                popupAnchor: [0, 0]
+            },
+            type: 'weather',
+            url: 'http://www.shipxy.com/Content/js/elanemap/images/yin.png'
+        },
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [112.5, 20.5]
+        }
+    }]
+}]
